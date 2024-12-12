a9e6b264f3fbab15782e8e608bd65dc4154d5752
QuentinGP <quentgeoffpro@yahoo.com>
2024-12-12 15:35:28 +0100

    third commit

--- index.test.js
+++ index.test.js
@@ -1,3 +1,11 @@
-const isIsogram = require(".");
+const isIsogram = require("./index");
 
+test("Tests for exercice", () => {
+  expect(isIsogram("Dermatoglyphics")).toBe(true);
+  expect(isIsogram("isIsogram")).toBe(false);
+  expect(isIsogram("isogram")).toBe(true);
+  expect(isIsogram("moOse")).toBe(false);
+  expect(isIsogram("aba")).toBe(false);
+  expect(isIsogram("")).toBe(true);
+});
 test("Tests for exercice", () => {});
