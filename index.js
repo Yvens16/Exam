84e0cfdba3f550fc5cf6c19f0948317a4dc72e12
Yvens16 <yvensbelaston@gmail.com>
2024-12-12 09:23:04 +0100

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.   
+
+const isIsogram = str => {
+  // Your solution
+};
+
+console.log(isIsogram('Dermatoglyphics')); // true
+console.log(isIsogram('isIsogram')); // false
+console.log(isIsogram('isogram')); // true
+console.log(isIsogram('moOse')); // false
+console.log(isIsogram('aba')); // false
+console.log(isIsogram('')); // true
+
+ * 
+ * 
+ */
+
+function isIsogram (word) {
+  console.log('first')
+}
+
+module.exports = isIsogram
