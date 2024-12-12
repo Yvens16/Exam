45f91b9a231d6d2858cbee3bac8a1bdb32e08b76
Alice Lemerre <al.lemerre@laposte.net>
2024-12-12 12:20:04 +0100

    exam

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-/**
- * 
- * Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.   
+
+ /*Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.   
 
 const isIsogram = str => {
   // Your solution
@@ -12,13 +11,41 @@ console.log(isIsogram('isogram')); // true
 console.log(isIsogram('moOse')); // false
 console.log(isIsogram('aba')); // false
 console.log(isIsogram('')); // true
-
- * 
- * 
- */
-
+ 
 function isIsogram (word) {
   console.log('first')
 }
+*/
 
-module.exports = isIsogram
+function isIsogram(word) {
+  word = toLowerCase(word);
+
+  if (word == ""){
+    return true;
+  }
+
+  let i;
+  let j;
+
+  word = word.split("");
+
+  for (i = 0; i < word.length; i++){
+    for(j = i + 1; j < word.length; ++j) {
+      if(word[i] === word[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+export default isIsogram;
+
+
+console.log(isIsogram('Dermatoglyphics')); // true
+console.log(isIsogram('isIsogram')); // false
+console.log(isIsogram('isogram')); // true
+console.log(isIsogram('moOse')); // false
+console.log(isIsogram('aba')); // false
+console.log(isIsogram('')); // true
+
--- index.test.js
+++ index.test.js
@@ -1,3 +1,11 @@
 const isIsogram = require('.');
 
-test('Tests for exercice', () => {})
+test('Tests for exercice', () => {
+    expect(isIsogram('Dermatoglyphics')).toBe(true);
+    expect(isIsogram('isIsogram')).toBe(false);
+    expect(isIsogram('moOse')).toBe(false);
+    expect(isIsogram('aba')).toBe(false);
+    expect(isIsogram('')).toBe(false);
+})
+
+
