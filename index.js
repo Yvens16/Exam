b50d00e4bf2de684599effee9089e8adc8a07414
jihad <jihad.douhipro@gmail.com>
2024-12-12 13:43:46 +0100

    jihad

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const isIsogram = str => {
   // Your solution
 };
 
+
+- tout mettre dans un tableau
+- la même lettre qu'elle soit miniscule ou majuscule = même chose // ~ upperCaseLow 
+- filtrer (.filter) le tableau pour chercher les mots avec les mêmes lettres (majuscule & minuscule confondus) en mettant une boucle "for" (ou "switch") 
+- retourner la fonction/tableau 
+
+
 console.log(isIsogram('Dermatoglyphics')); // true
 console.log(isIsogram('isIsogram')); // false
 console.log(isIsogram('isogram')); // true
@@ -17,8 +24,15 @@ console.log(isIsogram('')); // true
  * 
  */
 
+let tableIso = ['Dermatoglyphics','isIsogram','isogram','moOse','aba' ]
+
 function isIsogram (word) {
+  tableIso.filter()
+  
   console.log('first')
 }
 
+
 module.exports = isIsogram
+
+
