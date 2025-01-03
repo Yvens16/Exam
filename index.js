4220523a78142e7dda59d7db213877ead96eaf7c
Rayan Rnithiananthan <rayan.nithian@gmail.com>
2025-01-03 11:31:55 +0100

    Exam

--- index.js
+++ index.js
@@ -14,8 +14,25 @@ console.log(isIsogram('')); // true
  * 
  */
 
-function isIsogram (word) {}
+function isIsogram (word) {
+    const wordLower = word.toLowerCase();
+    for(let i = 0; i < wordLower.length; i++){
+        for(let j = i + 1; j < wordLower.length; j++){
+            
+            if(wordLower[i] === wordLower[j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
+console.log(isIsogram('Dermatoglyphics'))
+console.log(isIsogram('isIsogram'))
+console.log(isIsogram('isogram'))
+console.log(isIsogram('moOse'))
+console.log(isIsogram('aba'))
+console.log(isIsogram(''))
 
 module.exports = isIsogram
 
--- index.test.js
+++ index.test.js
@@ -1,3 +1,25 @@
 const isIsogram = require('.');
 
-test('Tests for exercice', () => {})
+test('Le mot Dermatoglyphics renvoie True ', () => {
+    expect(isIsogram("Dermatoglyphics")).toBe(true);
+})
+
+test('Le mot isIsogram renvoie True ', () => {
+    expect(isIsogram("isIsogram")).toBe(false);
+})
+
+test('Le mot isogram renvoie True ', () => {
+    expect(isIsogram("isogram")).toBe(true);
+})
+
+test('Le mot moOse renvoie True ', () => {
+    expect(isIsogram("moOse")).toBe(false);
+})
+
+test('Le mot aba renvoie True ', () => {
+    expect(isIsogram("aba")).toBe(false);
+})
+
+test('une chaine de caractere vide renvoie True ', () => {
+    expect(isIsogram("")).toBe(true);
+})
