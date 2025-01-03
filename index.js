336a2c234181177f0bec82d4010b8c12e1db933f
Marin <marin.maulbon@perfectogrupe.fr>
2025-01-03 09:52:51 +0100

    exam

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 /**
- * 
- * Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.   
-
-const isIsogram = str => {
-  // Your solution
-};
-
-
-
-console.log(isIsogram('Dermatoglyphics')); // true
-console.log(isIsogram('isIsogram')); // false
-console.log(isIsogram('isogram')); // true
-console.log(isIsogram('moOse')); // false
-console.log(isIsogram('aba')); // false
-console.log(isIsogram('')); // true
-
- * 
- * 
+ *
+ * Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.
+ *
+ *
  */
+function isIsogram(str) {
+  // Your solution
+  const splitWord = str.split("");
+  let oldLetters = [splitWord[0]];
+  for (i = 1; i < splitWord.length; i++) {
+    if (splitWord[i].toUpperCase != splitWord[i - 1].toUpperCase) {
+      return false;
+    } else if (splitWord[i].toUpperCase != splitWord[i - 1].toUpperCase) {
+      for (i = 0; i < oldLetters.length; i++) {
+        if (oldLetters[i].toUpperCase === splitWord[i].toUpperCase) {
+          return false;
+        }
+      }
+    }
+    oldLetters.push(splitWord[i]);
+  }
 
-function isIsogram (word) {}
+  return true;
+}
 
+isIsogram("Dermatoglyphics");
 
-module.exports = isIsogram
+// console.log(isIsogram("Dermatoglyphics")); // true
+// console.log(isIsogram("isIsogram")); // false
+// console.log(isIsogram("isogram")); // true
+// console.log(isIsogram("moOse")); // false
+// console.log(isIsogram("aba")); // false
+// console.log(isIsogram("")); // true
 
-
+module.exports = isIsogram;
