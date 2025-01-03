8de7731954404fac3c91345da8b12e3227a22b51
Jason MENNECHET <ripseya@hotmail.com>
2025-01-03 11:03:41 +0100

    exam Jason

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-/**
- * 
- * Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.   
+// Un isogramme est un mot qui n’a pas de lettres qui se répète, consécutivement	ou non. Ignorez la casse des lettres.
 
-const isIsogram = str => {
-  // Your solution
+const isIsogram = (str) => {
+  str = str.toLowerCase();
+
+  const lettres = str.split("");
+
+  for (let index = 0; index < lettres.length; index++) {
+    for (let j = index + 1; j < lettres.length; j++) {
+      if ((lettres[index] === lettres[j])) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
 
+console.log(isIsogram("Dermatoglyphics")); // true
+console.log(isIsogram("isIsogram")); // false
+console.log(isIsogram("isogram")); // true
+console.log(isIsogram("moOse")); // false
+console.log(isIsogram("aba")); // false
+console.log(isIsogram("")); // true
 
-
-console.log(isIsogram('Dermatoglyphics')); // true
-console.log(isIsogram('isIsogram')); // false
-console.log(isIsogram('isogram')); // true
-console.log(isIsogram('moOse')); // false
-console.log(isIsogram('aba')); // false
-console.log(isIsogram('')); // true
-
- * 
- * 
- */
-
-function isIsogram (word) {}
-
-
-module.exports = isIsogram
-
-
+module.exports = isIsogram;
--- index.test.js
+++ index.test.js
@@ -1,3 +1,34 @@
-const isIsogram = require('.');
+const isIsogram = require('./index');
 
-test('Tests for exercice', () => {})
+// describe('Tests for exercice', () => {
+// })
+
+describe('Tests for exercice', () => {
+    test('Dermatoglyphics est un isogram', () => {
+        expect(isIsogram("Dermatoglyphics")).toBe(true);
+      });
+
+      test('isIsogram n\'est pas un isogram', () => {
+        expect(isIsogram("isIsogram")).toBe(false);
+      });
+
+      test('isogram est un isogram', () => {
+        expect(isIsogram("isogram")).toBe(true);
+      });
+
+      test('moOse n\'est pas un isogram', () => {
+        expect(isIsogram("moOse")).toBe(false);
+      });
+
+      test('aba n\'est pas un isogram', () => {
+        expect(isIsogram("moOse")).toBe(false);
+      });
+
+      test('aba n\'est pas un isogram', () => {
+        expect(isIsogram("moOse")).toBe(false);
+      });
+
+      test('"" renvoie true', () => {
+        expect(isIsogram("")).toBe(true);
+      });
+  });
